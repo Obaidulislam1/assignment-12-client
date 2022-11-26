fa7be052354aa04eda47d931975d663171a2ada2
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-26 08:40:16 +0600

    product page added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Carusale from '../page/Carusale';
+import Product from '../page/Product';
 import Section from '../page/Section';
 
 const Home = () => {
     return (
         <div>
             <Carusale></Carusale>
+            <Product></Product>
             <Section></Section>
         </div>
     );
---
+++ src/components/page/Product.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Product = () => {
+    return (
+        <div>
+            <div>
+                <h1 className='text-3xl font-bold text-center'>Our Product</h1>
+                <p className='text-center font-bold'>Mobile phones started to become popular in Bangladesh in the late 90s. The first phones were way oversized compared to any phone of today. There was an antenna on top of the phone, and they used to be quite troublesome to carry because of their weight.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
