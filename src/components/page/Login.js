05f6128b3f57edb9283412a4a8c79d2ace5af1e0
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-26 23:49:40 +0600

    login page start

--- src/components/page/Login.js
+++ src/components/page/Login.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import image from '../../image/login.jpg'
 
 const Login = () => {
+    const handleLogin = event =>{
+        event.preventdefault()
+        const form = event.target;
+    }
     return (
-        <div>
-            <h1>login page</h1>
+        <div className="hero w-full">
+            <div className="hero-content grid md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left mx-auto">
+                    <img src={image} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 mx-auto">
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name='email' type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name='password' type="text" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
