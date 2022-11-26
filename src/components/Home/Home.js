1db4d8c54dee19ce6e4a1f6a3f4c2ec3b5618f88
obaidul islam1 <mursalinislam537215@gmail.com>
2022-11-26 08:29:53 +0600

    data manage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Carusale from '../page/Carusale';
+import Section from '../page/Section';
 
 const Home = () => {
     return (
         <div>
             <Carusale></Carusale>
+            <Section></Section>
         </div>
     );
 };
